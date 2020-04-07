1985b4de68f28a3f56623b9fb8d3802b9b2f5113
Jaroslav Malec <dzardacz@gmail.com>
2020-04-08 01:10:59 +0200

    Add the CDC loopback demo, adjusted

--- fw/rbcx-coprocessor/src/main.cpp
+++ fw/rbcx-coprocessor/src/main.cpp
@@ -1,27 +1,31 @@
-
-
 #include "stm32f1xx_hal.h"
 #include "stm32f1xx_hal_gpio.h"
-// #include "usb_device.h"
 
+#include "usb_cdc_link.h"
 
-void SystemClock_Config(void);
-static void GPIO_Init(void);
-void Error_Handler(void);
+void SystemClock_Config();
+void pins_init();
+extern "C" void Error_Handler();
 
 int main()
 {
   SystemClock_Config();
-  GPIO_Init();
+  HAL_Init();
+  pins_init();
+  cdc_link_init();
+  while (true)
+  {
+    cdc_link_poll();
+  }
 }
 
-void SystemClock_Config(void)
+void SystemClock_Config()
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;  
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
@@ -33,8 +37,7 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -52,42 +55,32 @@ void SystemClock_Config(void)
   }
 }
 
-static void GPIO_Init(void)
+void pins_init()
 {
+  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  auto init = GPIO_InitTypeDef {0};
+  auto init = GPIO_InitTypeDef{0};
   init.Pin = GPIO_PIN_13;
   init.Mode = GPIO_MODE_OUTPUT_PP;
   init.Pull = GPIO_NOPULL;
   init.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &init);
   HAL_GPIO_WritePin(GPIOC, 13, GPIO_PIN_SET);
+
+  init.Pin = GPIO_PIN_11 | GPIO_PIN_12;
+  init.Mode = GPIO_MODE_AF_PP;
+  init.Pull = GPIO_NOPULL;
+  init.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &init);
 }
 
-void Error_Handler(void)
+extern "C" void Error_Handler()
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-
-  /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{ 
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+extern "C" void SysTick_Handler()
+{
+  HAL_IncTick();
 }
-#endif /* USE_FULL_ASSERT */
